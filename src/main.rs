41b5e75dab3e4640c2cdd1ce778ad9a17a8da339
est31 <MTest31@outlook.com>
2021-02-13 10:01:12 +0100

    Add live stream mode

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use yasna::Tag;
 use yasna::models::ObjectIdentifier as Oid;
 use clap::Clap;
+use ctclient::CTClient;
 
 fn list_cert_extensions(cert_pem :&str) -> Result<Vec<Oid>> {
 	let der = pem::parse(cert_pem)?;
@@ -97,11 +98,7 @@ struct Opts {
 enum SubCommand {
 	ListExt(ListExtOpts),
 	Dl(DlOpts),
-}
-
-#[derive(Clap)]
-struct DlOpts {
-	url :String,
+	LiveStream(LstrOpts),
 }
 
 #[derive(Clap)]
@@ -110,12 +107,25 @@ struct ListExtOpts {
 	pem_file :String,
 }
 
+#[derive(Clap)]
+struct DlOpts {
+	url :String,
+}
+
+#[derive(Clap)]
+struct LstrOpts {}
+
 static USER_AGENT :&str = concat!("ct-ext-search ", env!("CARGO_PKG_VERSION"),
 	". https://github.com/est31/ct-ext-search");
 
 fn main() -> Result<()> {
 	let opts :Opts = Opts::parse();
 	match opts.op {
+		SubCommand::ListExt(opts) => {
+			let pem = std::fs::read_to_string(&opts.pem_file)?;
+			let oids = list_cert_extensions(&pem)?;
+			println!("{:?}", oids);
+		},
 		SubCommand::Dl(opts) => {
 			println!("Downloading from log at {}", opts.url);
 			let client = reqwest::blocking::Client::builder()
@@ -124,10 +134,24 @@ fn main() -> Result<()> {
 			let res = client.get(&format!("{}/ct/v1/get-sth", opts.url)).send()?;
 			println!("{}", res.text()?);
 		},
-		SubCommand::ListExt(opts) => {
-			let pem = std::fs::read_to_string(&opts.pem_file)?;
-			let oids = list_cert_extensions(&pem)?;
-			println!("{:?}", oids);
+		SubCommand::LiveStream(opts) => {
+			let public_key = base64::decode("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAETeBmZOrzZKo4xYktx9gI2chEce3cw/tbr5xkoQlmhB18aKfsxD+MnILgGNl0FOm0eYGilFVi85wLRIOhK8lxKw==").unwrap();
+			const URL: &str = "https://ct.googleapis.com/logs/argon2021/";
+			// TODO convertability of the error, TODO user agent, TODO library neutral means of access
+			let mut client = CTClient::new_from_latest_th(URL, &public_key).unwrap();
+			loop {
+				let update_result = client.update(Some(|certs: &[openssl::x509::X509]| {
+					for c in certs {
+						let der = c.to_der().unwrap();
+						let oids = list_cert_extensions_der(&der).unwrap();
+						println!("{:?}", oids);
+					}
+				}));
+				if update_result.is_err() {
+					eprintln!("Error: {}", update_result.unwrap_err());
+					break;
+				}
+			}
 		},
     }
 	Ok(())
